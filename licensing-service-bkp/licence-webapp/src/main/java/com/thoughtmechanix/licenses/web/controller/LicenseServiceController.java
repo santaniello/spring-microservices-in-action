6493d2bdee7ff28595463d52047b97f39abbf6c4
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2019-05-26 16:35:38 -0300

    add novas alterações

---
+++ licensing-service-bkp/licence-webapp/src/main/java/com/thoughtmechanix/licenses/web/controller/LicenseServiceController.java
@@ -0,0 +1,19 @@
+package com.thoughtmechanix.licenses.web.controller;
+
+
+import com.thoughtmechanix.core.model.License;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+    @GetMapping(value="/{licenseId}")
+    public License getLicenses(
+                @PathVariable("organizationId") String organizationId,
+                @PathVariable("licenseId")      String licenseId) {
+        return new License(licenseId, "Teleco", "Seat","TestOrg");
+    }
+}
---
+++ licensing-service/license-core/src/main/java/model/License.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class License {
+    private String id;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
+}
